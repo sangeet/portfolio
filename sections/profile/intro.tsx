c41a95f33104fceeaa2312c6ce196d0453f09ffd
Sangeet Khatri <sangeet.khatri@outlook.com>
2025-06-25 22:35:42 +0100

    Updating location and contact

--- sections/profile/intro.tsx
+++ sections/profile/intro.tsx
@@ -7,11 +7,11 @@ const IntroBlock = () => {
                 <h1 className="mb-3 font-bold text-2xl md:text-3xl lg:text-4xl">
                     Sangeet Khatri
                 </h1>
-                <span className="text-xl md:text-2xl">Frontend Developer</span>
-                <span className="text-sm">Bengaluru, Karnataka, India</span>
+                <span className="text-xl md:text-2xl">Full Stack Developer</span>
+                <span className="text-sm">Setubal, Portugal</span>
             </div>
             <div className="flex flex-col md:text-right mt-5">
-                <span className="text-2xl">+91-85532 21820</span>
+                <span className="text-2xl">+351 910 749 525</span>
                 <span className="mt-1">sangeet.khatri@outlook.com</span>
                 <div className="flex flex-wrap justify-end gap-3 mt-5">
                     <SocialIcons/>
